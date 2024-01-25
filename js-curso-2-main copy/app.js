91ac11f0dfb37106c4741852ef5ac0b2e4bb4668
Yuto018 <157655779+Yuto018@users.noreply.github.com>
2024-01-24 23:16:58 -0500

    Patomomo

---
+++ js-curso-2-main copy/app.js
@@ -0,0 +1,86 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Quien es el mas kbro?';
+function clickConsole(){
+    alert('Yo pipibts no soy');
+}
+function clickPromt() {
+    alert('Eres kbro te escupo');
+}
+
+function cliclAlert() {
+    alert('Si, es el mas kbro');
+}
+
+function clickSuma(){
+    alert('Tengo un causa kbro');
+}
+function hola(){
+    console.log('hola mundo');
+}
+/*
+function multiplicador(){
+    let numero1 = prompt('Ingrese un numero:');
+    let resultado = 0;
+    resultado = numero1*2;
+    console.log(resultado);
+    return
+}
+multiplicador();
+*/
+
+/*
+function promedio(){
+    contador1=0;
+    let suma2 = 0;
+    let pro = 0;
+    while(contador1<3){
+        contador1++;
+        let numero2 = parseInt(prompt('Ingresa numero a sumar:'));
+        suma2 = numero2 + suma2;
+    }
+    pro = suma2/3;
+    console.log(pro);
+}
+*/
+
+// Falta por hacer 
+//Crear una función que reciba dos números como parámetros y devuelva el mayor de ellos.
+//Crear una función que reciba un número como parámetro y devuelva el resultado de multiplicar ese número por sí mismo.
+//promedio();
+ 
+/*
+let indice = 0;
+
+function imc(){
+    let altura = prompt('Ingrese su altura en metros: ');
+    let peso = prompt('Ingrese su peso en kilogramos: ');
+    indice = peso/(altura*altura);
+    let redondeado = indice.toFixed(2);
+    alert(`Su indice de masa corporal es: ${redondeado}`);
+    return redondeado;
+
+    
+}
+
+
+
+imc();
+*/
+/*
+function factorial() {
+    let numero = prompt('Ingrese el numero: ');
+    let fac = 1;
+    let contador = numero;
+    while(contador>0) {
+        fac = fac*numero; 
+        contador = contador - 1;
+        numero = numero -1;
+    }
+    alert(`el numero es: ${fac}`);
+    return
+}
+
+factorial();
+
+*/
+
